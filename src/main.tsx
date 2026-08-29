c14e1736a48a4160e200a809bfa6e3b64ec5ad18
andreskepple <andre.skepple@fullspektrum.co.uk>
2026-08-29 05:50:35 +0100

    Import canonical fidelity layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './canonical-shell.css'
+import './canonical-fidelity.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
