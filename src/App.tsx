6592d9bf47ef70349090df302d6a40ae7076fd33
andreskepple <andre.skepple@fullspektrum.co.uk>
2026-08-29 05:21:03 +0100

    fix: align router basename with deployment base

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import Learn from "./pages/Learn";
 export default function App() {
   return (
     <AppProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route element={<AppShell />}>
             <Route path="/" element={<Home />} />
