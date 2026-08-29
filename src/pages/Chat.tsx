d419b3e26860510ac3f7f40473750ec2b529f2b3
andreskepple <andre.skepple@fullspektrum.co.uk>
2026-08-29 05:44:21 +0100

    Use canonical chat page structure

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -3,10 +3,9 @@ import { useApp } from "@/store";
 import TopBar from "@/components/TopBar";
 import Messages from "@/components/Messages";
 import Composer from "@/components/Composer";
-import StatusFooter from "@/components/StatusFooter";
 
 export default function Chat() {
-  const { messages, sendMessage } = useApp();
+  const { messages } = useApp();
   const ctx = useOutletContext<{ openSidebar: () => void }>();
 
   return (
@@ -18,21 +17,18 @@ export default function Chat() {
         onMenuOpen={ctx?.openSidebar}
       />
 
-      {/* Desktop: messages + inline composer + footer */}
-      <div className="hidden lg:flex flex-col flex-1 min-h-0">
+      <div className="hidden lg:flex flex-col flex-1 min-h-0 canonical-chat-only">
         <Messages messages={messages} />
-        <div className="flex justify-center py-4 px-4">
+        <div className="canonical-input-zone">
           <Composer />
         </div>
-        <StatusFooter />
       </div>
 
-      {/* Mobile: scrollable messages + floating composer */}
       <div className="flex lg:hidden flex-col flex-1 min-h-0 relative">
         <div className="flex-1 overflow-y-auto pb-[140px]">
           <Messages messages={messages} embed />
         </div>
-        <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-28 z-10"
+        <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-48 z-10"
           style={{ background: "linear-gradient(to top, var(--ws-to), transparent)" }} />
         <div className="absolute bottom-0 left-0 right-0 z-20 pb-4 pt-2">
           <Composer mobile />
